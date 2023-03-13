6c0c36579a99f05b681a778462403423c8dac316
RubenAcosta224 <124905283+RubenAcosta224@users.noreply.github.com>
2023-03-13 09:51:20 -0600

    Add files via upload

---
+++ EVA2_9_SUPER_2/src/eva2_9_super_2/Estudiante.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+
+//Clase derivada extends clase base
+//subclase extends superclase
+//hijo extends clase
+
+package eva2_9_super_2;
+
+/**
+ *
+ * @author invitado
+ */
+public class Estudiante extends Persona {
+    private String noControl;
+
+    public String getNoControl() {
+        return noControl;
+    }
+
+    public void setNoControl(String noControl) {
+        this.noControl = noControl;
+    }
+
+
+    
+    
+}
---
+++ EVA2_9_SUPER_2/src/eva2_9_super_2/Proveedores.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_9_super_2;
+
+/**
+ *
+ * @author invitado
+ */
+
+
+//proveedores is-a Persona
+public class Proveedores extends Persona {
+    private String rfc;
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+    
+    
+}
